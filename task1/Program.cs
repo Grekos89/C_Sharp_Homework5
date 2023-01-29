4b85c5ad1745661b34fa7a8f7dbcbdc288a5d350
Grekos89 <70657407+Grekos89@users.noreply.github.com>
2023-01-29 15:50:36 +0300

    задача 34

---
+++ task1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+//которая покажет количество чётных чисел в массиве.
+
+int[] newArr = RandomArray(10);
+PrintArray(newArr);
+System.Console.WriteLine(GetMeEven(newArr));
+
+
+int GetMeEven(int[] arr)
+{
+    int count = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+
+int[] RandomArray(int length)
+{
+     int[]arr = new int[length];
+     for(int i = 0; i < length; i++)
+     {
+         arr[i] = new Random().Next(100,1000);
+     }
+     return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    
+    foreach(int item in arr)
+    {
+        System.Console.Write($"{item}, ");
+    } 
+    System.Console.WriteLine();
+}
